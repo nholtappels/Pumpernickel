f25046da406f56244858c29b676837c410c225b5
nholtappels <niclas@holtappels.in>
2013-12-16 20:39:51 +0100

    some unnecessary files deleted

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -6,12 +6,19 @@ Created on 15.12.2013
 
 from csvPreprocess import csvPreprocess as pp
 
-prep = pp()
+# look at KAPPA
+# implement upper threshold
+# association measure
+
+frequency_threshold = 15
+numlines = 10000
+
+prep = pp(frequency_threshold, numlines)
 
 csv = 'train.csv'
-storage = 'training_data_3'
-features_csv = 'features_2.csv'
-targets_csv = 'targets_2.csv'
+storage = 'training_data_' + str(frequency_threshold) + '_' + str(numlines)
+features_csv = 'features_' + str(frequency_threshold) + '_' + str(numlines) + '.csv'
+targets_csv = 'targets_' + str(frequency_threshold) + '_' + str(numlines) + '.csv'
 
 prep.import_csv(csv, storage)
 prep.create_new_csvs(features_csv, targets_csv, storage)
